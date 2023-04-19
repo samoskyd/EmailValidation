f11c9e8582a42ea9f3c769b29895a83e534ef5b5
Максим Микитюк <maks.mykytyuk@gmail.com>
2023-04-19 10:01:49 +0300

    upd

--- EmailValidation/Controllers/EmailController.cs
+++ EmailValidation/Controllers/EmailController.cs
@@ -4,6 +4,7 @@ using MailKit.Net.Smtp;
 using MailKit;
 using MimeKit;
 using System.Text;
+using System.Diagnostics.Contracts;
 
 namespace EmailValidation.Controllers
 {
@@ -18,10 +19,19 @@ namespace EmailValidation.Controllers
             _logger = logger;
         }
 
+        [ContractAbbreviator]
+        private void EnsureStringNotNull(string str)
+        {
+            Contract.Requires(str != null);
+        }
+
+
+        [ContractVerification(true)]
         [HttpPost(Name = "CheckEmail")]
         public string Post(string email)
         {
             // check validity
+            EnsureStringNotNull(email);
             try
             {
                 var eAddress = new MailAddress(email);
@@ -33,14 +43,19 @@ namespace EmailValidation.Controllers
 
             // generate code
             string pr_code = GenerateCode();
+            Contract.Requires(!string.IsNullOrEmpty(pr_code));
 
+            Contract.Ensures(Contract.Result<string>() != null);
             // send email
             if (!SendEmail(email, pr_code)) return "";
 
             // send response
+            Contract.Assert(!string.IsNullOrEmpty(email));
             return pr_code;
         }
 
+
+        [ContractVerification(true)]
         private string GenerateCode() 
         {
             Random _random = new Random();
@@ -52,14 +67,19 @@ namespace EmailValidation.Controllers
             for (var i = 0; i < 8; i++)
             {
                 var @char = (char)_random.Next(offset, offset + lettersOffset);
+                Contract.Assert(char.IsLetter(@char));
                 builder.Append(@char);
             }
 
+            Contract.Ensures(Contract.Result<string>() != null);
             return builder.ToString();
         }
 
+        [ContractVerification(true)]
         private bool SendEmail(string email, string code)
         {
+            Contract.Requires(!string.IsNullOrEmpty(email));
+            Contract.Requires(!string.IsNullOrEmpty(code));
             var message = new MimeMessage();
 
             message.From.Add(new MailboxAddress("Helper App", "apphelper.f@ukr.net"));
@@ -71,12 +91,18 @@ namespace EmailValidation.Controllers
                 Text = "Please, enter this code in Helper App to verify your email: " + code
             };
 
+            Contract.Requires(message != null);
             using (var smtp = new MailKit.Net.Smtp.SmtpClient())
             {
-                string smtpName = System.Environment.GetEnvironmentVariable("smtpName");
-                string smtpPort = System.Environment.GetEnvironmentVariable("smtpPort");
-                string maillog = System.Environment.GetEnvironmentVariable("maillog");
-                string mailpass = System.Environment.GetEnvironmentVariable("mailpass");
+                string smtpName = Environment.GetEnvironmentVariable("smtpName");
+                string smtpPort = Environment.GetEnvironmentVariable("smtpPort");
+                string maillog = Environment.GetEnvironmentVariable("maillog");
+                string mailpass = Environment.GetEnvironmentVariable("mailpass");
+
+                EnsureStringNotNull(smtpName);
+                EnsureStringNotNull(smtpPort);
+                EnsureStringNotNull(maillog);
+                EnsureStringNotNull(mailpass);
 
                 smtp.Connect(smtpName, Convert.ToInt32(smtpPort));
                 smtp.Authenticate(maillog, mailpass);
